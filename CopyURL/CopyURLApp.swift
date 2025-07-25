288d292265b886230fde5838033b9245a5ca6907
Rémino Rem <29999+remino@users.noreply.github.com>
2025-07-25 22:26:23 +0900

    Initial Commit

---
+++ CopyURL/CopyURLApp.swift
@@ -0,0 +1,17 @@
+//
+//  CopyURLApp.swift
+//  CopyURL
+//
+//  Created by V on 2025-07-25.
+//
+
+import SwiftUI
+
+@main
+struct CopyURLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CopyURLTests/CopyURLTests.swift
@@ -0,0 +1,17 @@
+//
+//  CopyURLTests.swift
+//  CopyURLTests
+//
+//  Created by V on 2025-07-25.
+//
+
+import Testing
+@testable import CopyURL
+
+struct CopyURLTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
