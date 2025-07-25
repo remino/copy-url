ff4a26247969efce0d634416dac1f165f776da9b
Rémino Rem <29999+remino@users.noreply.github.com>
2025-07-25 23:24:44 +0900

    Copy URL then exit

--- CopyURL/CopyURLApp.swift
+++ CopyURL/CopyURLApp.swift
@@ -1,17 +1,32 @@
-//
-//  CopyURLApp.swift
-//  CopyURL
-//
-//  Created by V on 2025-07-25.
-//
-
 import SwiftUI
 
 @main
 struct CopyURLApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+	@NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+	
+	var body: some Scene {
+		Settings {} // No UI at all
+	}
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+	private var receivedURL = false
+
+	func application(_ application: NSApplication, open urls: [URL]) {
+		receivedURL = true
+		if let url = urls.first {
+			NSPasteboard.general.clearContents()
+			NSPasteboard.general.setString(url.absoluteString, forType: .string)
+			print("Copied to clipboard: \(url.absoluteString)")
+		}
+		NSApp.terminate(nil)
+	}
+
+	func applicationDidFinishLaunching(_ notification: Notification) {
+		DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+			if !self.receivedURL {
+				NSApp.terminate(nil)
+			}
+		}
+	}
 }
